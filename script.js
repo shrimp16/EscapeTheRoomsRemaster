9c3739e7ad3f4724cb10ae447312cae2f4337768
Shrimp <lcamara0916@gmail.com>
2022-02-21 13:03:47 -0100

    Some changes on the room data

--- script.js
+++ script.js
@@ -11,26 +11,24 @@ let html;
 let expectedPW = [];
 
 const bathroom = {
-    nextRoom: null,
+    /*nextRoom: null,
     buttons: ["toilet", "mirror", "shower"],
     buttonsText: ["Check the toilet", "Look on the mirror", "Check under the shower"],
-    buttonsReturn: ["You found nothing", "01000001 01110010 01100101 00100000 01111001 01101111 01110101 00100000 01100001 00100000 01100011 01100001 01110100 00111111", "All the clothes here seem to have something in common"], //testing propose
+    buttonsReturn: ["You found nothing", "", "All the clothes here seem to have something in common"], //testing propose
     images: [], // need to get the images
     passwords: ["null", "equals", "interface"],
     checked: [false, false, false],
-    mainImage: "/images/MainPage.png" // need to get image
+    mainImage: "/images/MainPage.png" // need to get image*/
 }
 
 const bedroom = {
     nextRoom: bathroom,
-    buttons: ["bed", "notebooks", "closet"],
+    buttons: ["Bed", "Notebooks", "Closet"],
     buttonsText: ["Check under the bed", "Check the old notebooks", "Check the closet"],
-    buttonsReturn: ["You found nothing", 
-    "01010011 01101111 01101101 01100101 01101111 01101110 01100101 00100000 01101011 01101001 01101100 01101100 01100101 01100100 00100000 01110100 01101000 01100101 00100000 01100011 01100001 01110100",
-    "Why do I only have jackets?"], //testing propose
-    secondText: ["There's nothing there", "Is it going to catch you too?", "This is full of jackets"],
+    firstText: ["I don't think there's anything there", "39321, 5031, 1203, 721435, 94769, 3009, 3, 365, 103, 323643, 787", "This is full of clothes"],
+    secondText: ["There's nothing there", "12393, 1305, 3021, 534127, 96749, 9003, 3, 563, 301, 346323, 787", "Oh! My super hero costume"],
     images: [], // need to get the images
-    passwords: ["null", "equals", "interface"],
+    passwords: ["null", "odd", "interface"],
     checked: [false, false, false],
     mainImage: "/images/MainPage.png" // need to get image
 }
@@ -60,7 +58,7 @@ function loadRoom() {
 
             roomTitle.innerHTML = `${currentRoom.buttons[i]}`;
 
-            createBanner(currentRoom.buttonsReturn[i]);
+            createBanner(currentRoom.firstText[i]);
             overlay.style.display = "flex";
 
             expectedPW.push(currentRoom.passwords[i]);
@@ -79,15 +77,14 @@ function loadRoom() {
 
 function createBanner(description) {
 
-    let words = description.split("");
     let char = 0;
+    let timer = setInterval(onTick, 75);
+    let words = description.split("");
     roomDescription.innerHTML = "";
-    console.log(words);
+    
     for (let i = 0; i < description.length; i++) {
         roomDescription.innerHTML += `<span>${description[i]}</span>`;
     }
-
-    let timer = setInterval(onTick, 75);
 
     function onTick() {
         const span = roomDescription.querySelectorAll('span')[char];
